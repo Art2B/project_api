3d0a97485118773996afa0b3ebafc0fdde239e86
Arthur Battut <arthur@soixantecircuits.fr>
2015-02-17 19:21:10 +0100

    enable delete a review in view

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,10 @@
 $('#js-delete').click(function(event) {
 	console.log('salut');
+  var id = $(this).data('id').substring(1).slice(0,-1);
+  var weburl = "reviews/delete/"+id;
+  console.log(weburl);
+  var request = new XMLHttpRequest();
+  request.open("DELETE", weburl, true);
+  request.send();
+  location.reload();
 });
